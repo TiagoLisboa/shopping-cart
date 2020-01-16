333319b5d4ea452ee46dc50d8b7c99acab11458f
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-01-16 14:03:19 -0300

    global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
+import ProductList from './componentes/ProductList';
+
 function App() {
   return (
     <div className="App">
+      <ProductList />
+      <GlobalStyle />
     </div>
   );
 }
