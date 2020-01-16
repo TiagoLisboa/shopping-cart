b6b47610326b0434ec96dc51f69360cd755fd54c
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-01-16 15:17:28 -0300

    mount prouct list with reducer data

--- src/components/ProductTable/index.js
+++ src/components/ProductTable/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { Container } from './styles';
 import { 
@@ -7,20 +8,24 @@ import {
 } from 'react-icons/fa';
 
 export default function ProductTable() {
+  const products = useSelector(state => state.cart);
   return (
     <Container>
       <table>
-        <tr>
-          <td>Apple</td>
-          <td>
-            <div>
-              <button type="button"><FaMinusCircle /></button> 
-              <input type="text" value={500} readOnly/> kg 
-              <button type="button"><FaPlusCircle /></button>
-            </div>
-          </td>
-          <td>$ 60</td>
-        </tr>
+        {products.map(product => (
+          <tr>
+            <td>{ product.name }</td>
+            <td>
+              <div>
+                <button type="button"><FaMinusCircle /></button> 
+                <input type="text" value={ product.quantity } readOnly/> kg 
+                <button type="button"><FaPlusCircle /></button>
+              </div>
+            </td>
+            <td>$ { product.value }</td>
+          </tr>
+        ))}
+        
       </table>
     </Container>
   );
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,15 @@
 export default function cart() {
-  return [];
+  return [{
+    name: 'Apple',
+    quantity: 0,
+    value: 60
+  },{
+    name: 'Bannana',
+    quantity: 0,
+    value: 10
+  }, {
+    name: 'Orange',
+    quantity: 0,
+    value: 120
+  }];
 }
