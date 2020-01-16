f6868b2576913dcf0fed416ed8c145f6334360e0
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-01-16 14:58:42 -0300

    inital redux setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Provider } from 'react-redux'
 
 import GlobalStyle from './styles/global';
-
 import ProductTable from './components/ProductTable';
 import CouponTable from './components/CouponTable';
 import Totals from './components/Totals';
 import CouponInput from './components/CouponInput';
 
+import store from './store';
+
 function App() {
   return (
-    <div className="App">
-      <ProductTable />
-      <CouponTable />
-      <Totals />
-      <CouponInput />
-      <GlobalStyle />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <ProductTable />
+        <CouponTable />
+        <Totals />
+        <CouponInput />
+        <GlobalStyle />
+      </div>
+    </Provider>
   );
 }
 
